7c40e4beb0ef35d539fa8c8ed4bd274196790faa
LaughingLeader <laughingleader@gmail.com>
2020-06-08 21:55:53 -0500

    Setup dominate undead with some beam effects / additional mechanics

--- Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/BootstrapServer.lua
@@ -1,7 +1,5 @@
 Ext.Require("Shared/Init.lua")
-
----@type TranslatedString
-local TranslatedString = LeaderLib.Classes["TranslatedString"]
+Ext.Require("Server/Skills/DominateUndead.lua")
 
 -- Ext.RegisterListener("ModuleLoading", function()
 
@@ -16,28 +14,3 @@ local TranslatedString = LeaderLib.Classes["TranslatedString"]
 Ext.RegisterConsoleCommand("hotbartest", function(cmd, ...)
 	Ext.BroadcastMessage("LLLICH_Debug_HotbarTest", "", nil)
 end)
-
-local resistedText = TranslatedString:Create("hdea60333g1649g444bgbf86g347676b8d581", "<font color='#C7A758' size='18'>Resisted [1]</font>")
-local immuneText = TranslatedString:Create("h2474259fg6d67g4c98ga77fg0164f1476a41", "<font color='#FF0058' size='18'>Immune to [1]</font>")
-
-function TryDominateUndead(target, source)
-	local minIntResistance = Ext.ExtraData["LLLICH_DominateUndead_IntResistance"] or 14
-	local intImmunity = Ext.ExtraData["LLLICH_DominateUndead_IntImmunity"] or 38
-	local duration = (Ext.ExtraData["LLLICH_DominateUndead_Turns"] or 3) * 6.0
-
-	local intelligence = NRD_CharacterGetComputedStat(target, "Intelligence", 0)
-
-	if intelligence < minIntResistance then
-		ApplyStatus(target, "CHARMED", duration, 1, source)
-	elseif intelligence < intImmunity then
-		if Ext.Random(0,100) <= 80 then
-			ApplyStatus(target, "CHARMED", duration, 1, source)
-		else
-			local text = resistedText.Value:gsub("%[1%]", Ext.GetTranslatedStringFromKey(Ext.StatGetAttribute("LLLICH_TRY_DOMINATE", "DisplayName")))
-			CharacterStatusText(target, text)
-		end
-	else
-		local text = immuneText.Value:gsub("%[1%]", Ext.GetTranslatedStringFromKey(Ext.StatGetAttribute("LLLICH_TRY_DOMINATE", "DisplayName")))
-		CharacterStatusText(target, text)
-	end
-end
---
+++ Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/Server/Skills/DominateUndead.lua
@@ -0,0 +1,35 @@
+---@type TranslatedString
+local TranslatedString = LeaderLib.Classes["TranslatedString"]
+
+local resistedText = TranslatedString:Create("hdea60333g1649g444bgbf86g347676b8d581", "<font color='#C7A758' size='18'>Resisted [1]</font>")
+local immuneText = TranslatedString:Create("h2474259fg6d67g4c98ga77fg0164f1476a41", "<font color='#FF0058' size='18'>Immune to [1]</font>")
+
+function ApplyDomination(target, source, duration)
+	if CharacterIsDead(target) == 1 then
+		CharacterResurrect(target)
+		ObjectSetFlag(target, "LLLICH_Dominate_WasDead", 0)
+	end
+	ApplyStatus(target, "LLLICH_UNDEAD_DOMINATED", duration, 1, source)
+end
+
+function TryDominateUndead(target, source)
+	local minIntResistance = Ext.ExtraData["LLLICH_DominateUndead_IntResistance"] or 14
+	local intImmunity = Ext.ExtraData["LLLICH_DominateUndead_IntImmunity"] or 38
+	local duration = (Ext.ExtraData["LLLICH_DominateUndead_Turns"] or 3) * 6.0
+
+	local intelligence = NRD_CharacterGetComputedStat(target, "Intelligence", 0)
+
+	if intelligence < minIntResistance then
+		ApplyDomination(target, source, duration)
+	elseif intelligence < intImmunity then
+		if Ext.Random(0,100) <= 80 then
+			ApplyDomination(target, source, duration)
+		else
+			local text = resistedText.Value:gsub("%[1%]", Ext.GetTranslatedStringFromKey(Ext.StatGetAttribute("LLLICH_TRY_DOMINATE", "DisplayName")))
+			CharacterStatusText(target, text)
+		end
+	else
+		local text = immuneText.Value:gsub("%[1%]", Ext.GetTranslatedStringFromKey(Ext.StatGetAttribute("LLLICH_TRY_DOMINATE", "DisplayName")))
+		CharacterStatusText(target, text)
+	end
+end
