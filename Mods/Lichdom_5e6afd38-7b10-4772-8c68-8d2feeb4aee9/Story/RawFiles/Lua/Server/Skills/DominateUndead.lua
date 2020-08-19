f9edffbe56ba5e497e972c4c63adf86d69addef5
LaughingLeader <laughingleader@gmail.com>
2020-08-19 17:41:01 -0500

    Tweaked Dominate Undead to work only in combat, if enemies are available

--- Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/Server/Skills/DominateUndead.lua
+++ Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/Server/Skills/DominateUndead.lua
@@ -12,24 +12,40 @@ function ApplyDomination(target, source, duration)
 	ApplyStatus(target, "LLLICH_DOMINATED", duration, 1, source)
 end
 
+local function CombatHasEnemies(target, combatid, player)
+	local combatDB = Osi.DB_CombatCharacters:Get(nil, combatid)
+	if combatDB ~= nil and #combatDB > 0 then
+		for i,v in pairs(combatDB) do
+			local char = GetUUID(v[1])
+			if char ~= target and char ~= player and CharacterIsEnemy(player, char) == 1 or CharacterIsAlly(char, target) == 1 then
+				return true
+			end
+		end
+	end
+	return false
+end
+
 function TryDominateUndead(target, source)
-	local minIntResistance = Ext.ExtraData["LLLICH_DominateUndead_IntResistance"] or 14
-	local intImmunity = Ext.ExtraData["LLLICH_DominateUndead_IntImmunity"] or 38
-	local duration = (Ext.ExtraData["LLLICH_DominateUndead_Turns"] or 3) * 6.0
-
-	local intelligence = NRD_CharacterGetComputedStat(target, "Intelligence", 0)
-
-	if intelligence < minIntResistance then
-		ApplyDomination(target, source, duration)
-	elseif intelligence < intImmunity then
-		if Ext.Random(0,100) <= 80 then
+	local combatId = CombatGetIDForCharacter(target)
+	if combatId ~= nil and CombatHasEnemies(GetUUID(target), combatId, GetUUID(source)) then
+		local minIntResistance = Ext.ExtraData["LLLICH_DominateUndead_IntResistance"] or 14
+		local intImmunity = Ext.ExtraData["LLLICH_DominateUndead_IntImmunity"] or 38
+		local duration = (Ext.ExtraData["LLLICH_DominateUndead_Turns"] or 3) * 6.0
+	
+		local intelligence = NRD_CharacterGetComputedStat(target, "Intelligence", 0)
+	
+		if intelligence < minIntResistance then
 			ApplyDomination(target, source, duration)
+		elseif intelligence < intImmunity then
+			if Ext.Random(0,100) <= 80 then
+				ApplyDomination(target, source, duration)
+			else
+				local text = resistedText.Value:gsub("%[1%]", Ext.GetTranslatedStringFromKey(Ext.StatGetAttribute("LLLICH_TRY_DOMINATE", "DisplayName")))
+				CharacterStatusText(target, text)
+			end
 		else
-			local text = resistedText.Value:gsub("%[1%]", Ext.GetTranslatedStringFromKey(Ext.StatGetAttribute("LLLICH_TRY_DOMINATE", "DisplayName")))
+			local text = immuneText.Value:gsub("%[1%]", Ext.GetTranslatedStringFromKey(Ext.StatGetAttribute("LLLICH_TRY_DOMINATE", "DisplayName")))
 			CharacterStatusText(target, text)
 		end
-	else
-		local text = immuneText.Value:gsub("%[1%]", Ext.GetTranslatedStringFromKey(Ext.StatGetAttribute("LLLICH_TRY_DOMINATE", "DisplayName")))
-		CharacterStatusText(target, text)
 	end
 end
