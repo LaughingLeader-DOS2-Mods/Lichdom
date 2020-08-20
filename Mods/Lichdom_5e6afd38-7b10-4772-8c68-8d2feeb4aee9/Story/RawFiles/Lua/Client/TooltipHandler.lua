c0786c2b1215df1ac6154d6271e50056b9742c9e
LaughingLeader <laughingleader@gmail.com>
2020-08-20 09:03:46 -0500

    Tweaked Drain Soul tooltip text

--- Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/Client/TooltipHandler.lua
+++ Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/Client/TooltipHandler.lua
@@ -4,6 +4,7 @@ local ts = Mods.LeaderLib.Classes.TranslatedString
 local LichAbility = ts:Create("hcbc6d3d0g7968g47a3gb6fcgf23c1a33d1c5", "<font color='#73F6FF'>Lichdom</font>")
 local PhylacteryAbility = ts:Create("h3750ae3bg4ef4g4fc1g9362gea05305d88c6", "<font color='#73F6FF'>Lichdom</font>")
 local SoulReaperRequirement = ts:Create("he95b6422g6fb0g4708g87b8g602aba339960", "Inflict Damage with a Necromancy Spell")
+local ShadowDamageHelper = ts:Create("hba763a56g971dg4dd3g85a4g7eb0addf0380", "Shadow damage bypasses armor and cannot be resisted.")
 
 ---@type table<string, TranslatedString>
 local AbilitySchool = {
@@ -35,10 +36,38 @@ local function OnSkillTooltip(character, skill, tooltip)
 				end
 			end
 		end
+	elseif skill == "Target_LLLICH_DrainSoul" then
+        local text,_ = Ext.GetTranslatedStringFromKey("LLLICH_Tooltip_OnKill")
+		local element = {
+            Type = "StatusBonus",
+            Label = text,
+        }
+        tooltip:AppendElement(element)
+        text,_ = Ext.GetTranslatedStringFromKey("LLLICH_Tooltip_DrainSoulBonus1")
+		element = {
+            Type = "StatusImmunity",
+            Label = text,
+        }
+        tooltip:AppendElement(element)
+        text,_ = Ext.GetTranslatedStringFromKey("LLLICH_Tooltip_DrainSoulBonus2")
+		element = {
+            Type = "StatusImmunity",
+            Label = text,
+        }
+        tooltip:AppendElement(element)
+        if Ext.StatGetAttribute(skill, "DamageType") == "Shadow" then
+            element = tooltip:GetElement("SkillProperties")
+            if element ~= nil then
+                table.insert(element.Properties, {
+                    Label = ShadowDamageHelper.Value,
+                    Warning = ""
+                })
+            end
+        end
 	end
 end
 
-local Phylactery_DisplayName = ts:Create("<font color='#73F6FF'>Phylactery ([1])</font>	h5ecb6e4cg6511g45a0g81fbga80332d73ec9")
+local Phylactery_DisplayName = ts:Create("h5ecb6e4cg6511g45a0g81fbga80332d73ec9", "<font color='#73F6FF'>Phylactery ([1])</font>")
 local Phylactery_Description = ts:Create("h84039498gaf60g4cfeg8675g5819d9a3d4c4", "<font color='#73F6FF'>The source of a Lich's immortality. Protect this at all cost.</font>") 
 local Phylactery_Empty = ts:Create("hf0641549g589ag4fe0g92b8g5d955ca084d9", "<font color='#FF0000'>Out of Souls</font>")
 local Phylactery_Charges = ts:Create("h36537a0bg24afg4727g9773gd4a9e1b036ed", "Souls")
