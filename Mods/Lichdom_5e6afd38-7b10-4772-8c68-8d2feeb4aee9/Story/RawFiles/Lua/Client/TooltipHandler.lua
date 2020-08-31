9b32ab5b1fdf52d6a06d8e4277a40f3beaabf906
LaughingLeader <laughingleader@gmail.com>
2020-08-31 12:17:43 -0500

    Setup some new tags with tooltip support

--- Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/Client/TooltipHandler.lua
+++ Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/Client/TooltipHandler.lua
@@ -72,43 +72,66 @@ local Phylactery_Description = ts:Create("h84039498gaf60g4cfeg8675g5819d9a3d4c4"
 local Phylactery_Empty = ts:Create("hf0641549g589ag4fe0g92b8g5d955ca084d9", "<font color='#FF0000'>Out of Souls</font>")
 local Phylactery_Charges = ts:Create("h36537a0bg24afg4727g9773gd4a9e1b036ed", "Souls")
 
+local WeaponTypeTags = {
+    "LLLICH_GRIMOIRE",
+    "LLLICH_LICHSTAFF",
+}
+
 ---@param item EclItem
 ---@param tooltip TooltipData
 local function OnItemTooltip(item, tooltip)
-    if item ~= nil and item:HasTag("LLLICH_Phylactery") then
-        print(item.StatsId, Ext.JsonStringify(item.WorldPos), Ext.JsonStringify(tooltip.Data))
-        local lichIsLooking = false
-        if Mods.LeaderLib.UI.ClientCharacter ~= nil then
-            local character = Ext.GetCharacter(Mods.LeaderLib.UI.ClientCharacter)
-            if character ~= nil then
-                lichIsLooking = character:HasTag("LLLICH_Lich")
+    if item ~= nil then
+        for i,tag in pairs(WeaponTypeTags) do
+            if item:HasTag(tag) then
+                local text,_ = Ext.GetTranslatedStringFromKey(tag)
+                local armorSlotType = tooltip:GetElement("ArmorSlotType")
+                if armorSlotType == nil then
+                    armorSlotType = {
+                        Type = "ArmorSlotType",
+                    }
+                    tooltip:AppendElement(armorSlotType)
+                end
+                armorSlotType.Label = text
+                break
             end
-        else
-            local characterStr = item:GetOwnerCharacter()
-            if characterStr ~= nil then
-                local character = Ext.GetCharacter(characterStr)
+        end
+        if item.StatsId == "WPN_UNIQUE_LLLICH_TwinSkullGrimoire_A" then
+
+        end
+        if item:HasTag("LLLICH_Phylactery") then
+            local lichIsLooking = false
+            if Mods.LeaderLib.UI.ClientCharacter ~= nil then
+                local character = Ext.GetCharacter(Mods.LeaderLib.UI.ClientCharacter)
                 if character ~= nil then
                     lichIsLooking = character:HasTag("LLLICH_Lich")
                 end
+            else
+                local characterStr = item:GetOwnerCharacter()
+                if characterStr ~= nil then
+                    local character = Ext.GetCharacter(characterStr)
+                    if character ~= nil then
+                        lichIsLooking = character:HasTag("LLLICH_Lich")
+                    end
+                end
             end
-        end
-        if not lichIsLooking then
-            local element = tooltip:GetElement("WandCharges")
-            if element ~= nil then
-                tooltip:RemoveElements("WandCharges")
-            end
-        else
-            local element = tooltip:GetElement("WandCharges")
-            if element ~= nil then
-                element.Label = Phylactery_Charges.Value
-            end
-            element = tooltip:GetElement("ItemRarity")
-            if element ~= nil then
-                element.Label = Phylactery_DisplayName
-            end
-            element = tooltip:GetElement("ItemDescription")
-            if element ~= nil then
-                element.Label = Phylactery_Description.Value
+            if not lichIsLooking then
+                local element = tooltip:GetElement("WandCharges")
+                if element ~= nil then
+                    tooltip:RemoveElements("WandCharges")
+                end
+            else
+                local element = tooltip:GetElement("WandCharges")
+                if element ~= nil then
+                    element.Label = Phylactery_Charges.Value
+                end
+                element = tooltip:GetElement("ItemRarity")
+                if element ~= nil then
+                    element.Label = Phylactery_DisplayName
+                end
+                element = tooltip:GetElement("ItemDescription")
+                if element ~= nil then
+                    element.Label = Phylactery_Description.Value
+                end
             end
         end
 	end
@@ -117,4 +140,5 @@ end
 Ext.RegisterListener("SessionLoaded", function()
     Game.Tooltip.RegisterListener("Skill", nil, OnSkillTooltip)
     Game.Tooltip.RegisterListener("Item", nil, OnItemTooltip)
+    Mods.LeaderLib.UI.RegisterItemTooltipTag("LLLICH_TWINSKULLS")
 end)
