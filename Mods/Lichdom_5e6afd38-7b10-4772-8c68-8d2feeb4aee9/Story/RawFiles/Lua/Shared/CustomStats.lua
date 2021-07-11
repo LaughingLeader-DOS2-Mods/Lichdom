267901a211af9344b3124efa2747844ef5226ffb
LaughingLeader <laughingleader@gmail.com>
2021-07-10 23:47:46 -0500

    Set up some initial custom stats with LeaderLib

---
+++ Mods/Lichdom_5e6afd38-7b10-4772-8c68-8d2feeb4aee9/Story/RawFiles/Lua/Shared/CustomStats.lua
@@ -0,0 +1,126 @@
+local isClient = Ext.IsClient()
+
+if isClient then
+	---@param id string
+	---@param stat CustomStatData
+	---@param character EclCharacter
+	---@param isVisible boolean
+	local function CanDisplayLichdomStat(id, stat, character, isVisible)
+		if Vars.DebugMode or character:HasTag("LLLICH_Lich") then
+			return true
+		elseif not isVisible then
+			return false
+		end
+	end
+
+	CustomStatSystem:RegisterStatVisibilityHandler("LLLICH_PhlyacteryLevel", CanDisplayLichdomStat)
+
+	local ts = Classes.TranslatedString
+
+	---@type table<integer,TranslatedString>
+	local phylacteryEffectText = {
+		[0] = ts:CreateFromKey("LLLICH_PhlyacteryLevel_None", "Your soul is not yet bound to an object."),
+		[1] = ts:CreateFromKey("LLLICH_PhlyacteryLevel_Effect1", "Gain a standard Phylactery with 3 charges."),
+		[2] = ts:CreateFromKey("LLLICH_PhlyacteryLevel_Effect2", "The phylactery gains 4 max charges."),
+		[3] = ts:CreateFromKey("LLLICH_PhlyacteryLevel_Effect3", "The phylactery gains 5 max charges."),
+		[4] = ts:CreateFromKey("LLLICH_PhlyacteryLevel_Effect4", "The phylactery gains 6 max charges."),
+	}
+
+	local phylacteryStatProgression = {
+		Amulet = {
+			--[0] = "LLLICH_Phylactery_Amulet",
+			[1] = "LLLICH_Phylactery_Amulet",
+			[2] = "LLLICH_Phylactery_Amulet_2",
+			[3] = "LLLICH_Phylactery_Amulet_3",
+			[4] = "LLLICH_Phylactery_Amulet_Final",
+		},
+		Ring = {
+			--[0] = "LLLICH_Phylactery_Ring",
+			[1] = "LLLICH_Phylactery_Ring",
+			[2] = "LLLICH_Phylactery_Ring_2",
+			[3] = "LLLICH_Phylactery_Ring_3",
+			[4] = "LLLICH_Phylactery_Ring_Final",
+		},
+		Smallbox = {
+			--[0] = "LLLICH_Phylactery_Smallbox",
+			[1] = "LLLICH_Phylactery_Smallbox",
+			[2] = "LLLICH_Phylactery_Smallbox_2",
+			[3] = "LLLICH_Phylactery_Smallbox_3",
+			[4] = "LLLICH_Phylactery_Smallbox_Final",
+		},
+		Jar = {
+			--[0] = "LLLICH_Phylactery_Jar",
+			[1] = "LLLICH_Phylactery_Jar",
+			[2] = "LLLICH_Phylactery_Jar_2",
+			[3] = "LLLICH_Phylactery_Jar_3",
+			[4] = "LLLICH_Phylactery_Jar_Final",
+		},
+	}
+
+	local ArmorBoostAttributes = {
+		MagicArmorBoost = "+%s Magic Armour",
+		Necromancy = "+%s Necromancy",
+		WaterSpecialist = "+%s Hydrosophist",
+	}
+
+	---@param character EclCharacter
+	---@param stat CustomStatData
+	---@param tooltip TooltipData
+	Game.Tooltip.RegisterListener("CustomStat", "LLLICH_PhlyacteryLevel", function(character, customStat, tooltip)
+		local element = tooltip:GetElement("AbilityDescription")
+		if element then
+			local value = math.min(4, customStat:GetValue(character))
+			if value >= 0 then
+				if value ~= 0 then
+					element.CurrentLevelEffect = LocalizedText.Tooltip.AbilityCurrentLevel:ReplacePlaceholders(value, phylacteryEffectText[value].Value)
+				else
+					element.CurrentLevelEffect = phylacteryEffectText[value].Value
+				end
+				if value ~= 4 then
+					element.NextLevelEffect = LocalizedText.Tooltip.AbilityNextLevel:ReplacePlaceholders(value+1, phylacteryEffectText[value+1].Value)
+				end
+
+				---@type string
+				local phylacteryType = ClientData.PhylacteryType[character.NetID] or "Amulet"
+				local statTable = phylacteryStatProgression[phylacteryType]
+				if statTable then
+					local statId = statTable[value]
+					if statId then
+						local stat = Ext.GetStat(statId)
+						if stat then
+							for attribute,valueStr in pairs(ArmorBoostAttributes) do
+								local attributeValue = stat[attribute]
+								if attributeValue and attributeValue > 0 then
+									tooltip:AppendElement({
+										Type="StatsTalentsBoost",
+										Label = string.format(valueStr, attributeValue)
+									})
+								end
+							end
+							if value ~= 4 then
+								local nextStatId = statTable[value+1]
+								if nextStatId then
+									local nextStat = Ext.GetStat(nextStatId)
+									if nextStat then
+										for attribute,valueStr in pairs(ArmorBoostAttributes) do
+											local attributeValue = nextStat[attribute]
+											local lastValue = stat[attribute] or 0
+											local diff = attributeValue - lastValue
+											if attributeValue and diff > 0 then
+												if not StringHelpers.IsNullOrWhitespace(element.NextLevelEffect) then
+													element.NextLevelEffect = string.format("%s<br>"..valueStr, element.NextLevelEffect, diff)
+												else
+													element.NextLevelEffect = string.format(valueStr, diff)
+												end
+											end
+										end
+									end
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end)
+end
